cc285ad1a86a3730832aabdb2ee9d630fcca2bb9
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-10-20 11:47:55 -0600

    Predict exercise completed and commented out

---
+++ iteration_testfile.rb
@@ -0,0 +1,12 @@
+=begin
+balances = [100, 49, 98, 73, 56]
+names = ["Cindy", "Oscar", "Rigo", "Joe", "Stephanie", "Tiff"]
+
+balances.each do |balance|
+  puts balance + 10
+end
+
+names.each do |name|
+  puts name.length
+end
+=end
